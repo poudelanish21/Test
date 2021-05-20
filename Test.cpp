64d532ec42911d2c28e9e42e4de9f2b567de40eb
Anish poudel <poudelanish17@gmail.com>
2021-05-20 17:39:49 +0545

    shit

--- Test.cpp
+++ Test.cpp
@@ -1,11 +1,55 @@
 #include <iostream>
 #include <Windows.h>
-#include "Dependencies/sms_dll.h"
+#include "sms_dll.h"
 
+class Enviroment {
+public:
 
+	std::string CurrentDirectory = "";
+	std::string UserName = "";
+
+	Enviroment() {
+
+		char UserName[25];
+
+		DWORD dwBuffer = 25;
+
+		GetUserName(UserName, &dwBuffer);
+
+		if (UserName == NULL) {
+
+			printMsg("\n\t\tAllocating Memory Failure!!!\t\t\n");
+
+			return;
+		}
+
+		this->UserName = UserName;
+
+		char* CurrentDirectory = (char*)malloc(256);
+
+		if (CurrentDirectory == 0) {
+
+			printMsg("\n\t\tAllocating Memory Failure\t\t\n");
+
+			return;
+		}
+
+		GetCurrentDirectory((DWORD)_msize(CurrentDirectory), CurrentDirectory);
+
+		this->CurrentDirectory = CurrentDirectory;
+
+		free(UserName);
+
+		free(CurrentDirectory);
+
+	}
+
+};
 
 int main()
 {
+
+
 
 	return ERROR_SUCCESS | GetLastError();
 
